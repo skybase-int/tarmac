214a27db315b4cec64b70abe784a742b9eaee1a5
incline <inclinedmind@proton.me>
2025-01-23 17:22:40 -0800

    add arbitrum subgraph urls

--- apps/webapp/src/lib/constants.ts
+++ apps/webapp/src/lib/constants.ts
@@ -138,6 +138,9 @@ export const STAGING_URL_SKY_SUBGRAPH_BASE =
 export const STAGING_URL_SKY_SUBGRAPH_BASE_TENDERLY =
   'https://query-subgraph-staging.sky.money/subgraphs/name/jetstreamgg/subgraph-baseTenderly';
 // TODO: Add these subgraph URLs once they're available
-export const PROD_URL_SKY_SUBGRAPH_ARBITRUM = '';
-export const STAGING_URL_SKY_SUBGRAPH_ARBITRUM = '';
-export const STAGING_URL_SKY_SUBGRAPH_ARBITRUM_TENDERLY = '';
+export const PROD_URL_SKY_SUBGRAPH_ARBITRUM =
+  'https://query-subgraph.sky.money/subgraphs/name/jetstreamgg/subgraph-arbitrum';
+export const STAGING_URL_SKY_SUBGRAPH_ARBITRUM =
+  'https://query-subgraph-staging.sky.money/subgraphs/name/jetstreamgg/subgraph-arbitrum';
+export const STAGING_URL_SKY_SUBGRAPH_ARBITRUM_TENDERLY =
+  'https://query-subgraph-staging.sky.money/subgraphs/name/jetstreamgg/subgraph-arbitrumTenderly';
