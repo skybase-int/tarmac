2c16dddb7518b307e15a804c05ea3d5e6ae24d39
iceanddust <iceanddust@proton.me>
2025-02-07 18:34:49 -0300

    Update to new Tenderly tetnet

--- packages/utils/src/getEtherscanLink.ts
+++ packages/utils/src/getEtherscanLink.ts
@@ -20,7 +20,7 @@ function getEtherscanPrefix(id: number) {
     case chainId.tenderlyBase:
       return 'dashboard.tenderly.co/explorer/vnet/376e4980-c2de-48b9-bf76-c25bd6d1c324';
     case chainId.tenderlyArbitrum:
-      return 'dashboard.tenderly.co/explorer/vnet/c67d99b5-9c23-429b-87f6-2e5e71326d53';
+      return 'dashboard.tenderly.co/explorer/vnet/5a2a28a6-322f-4506-acf8-1151a13b5ccf';
     default:
       return 'etherscan.io';
   }
