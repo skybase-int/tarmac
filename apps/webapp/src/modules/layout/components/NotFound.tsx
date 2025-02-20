1c4433ae433ae093b606dc1c7d029710451693a6
iceanddust <iceanddust@proton.me>
2025-02-20 15:42:49 -0300

    Improve 404 page

--- apps/webapp/src/modules/layout/components/NotFound.tsx
+++ apps/webapp/src/modules/layout/components/NotFound.tsx
@@ -1,8 +1,9 @@
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Layout } from './Layout';
 import { Heading, Text } from './Typography';
 import { Button } from '@/components/ui/button';
+import { NoResults } from '@jetstreamgg/widgets';
 
 export function NotFound() {
   const navigate = useNavigate();
@@ -15,17 +16,23 @@ export function NotFound() {
 
   return (
     <Layout>
-      <div className="my-6 text-center">
-        <Heading variant="large">404</Heading>
-        <Text>Page not found</Text>
-        <Text>
-          You will be redirected back to the homepage after 5 seconds or you can use the button below
-        </Text>
-        <Link to="/">
-          <Button variant="secondary" className="mt-4">
-            Back to homepage
+      <div className="-mt-16 flex w-full grow flex-col items-center justify-center text-center">
+        <div className="bg-container flex max-w-[650px] flex-col items-center gap-3 rounded-3xl border p-12 bg-blend-overlay backdrop-blur-[50px]">
+          <NoResults />
+          <Heading tag="h3" variant="large" className="text-[32px] leading-9">
+            Page not found
+          </Heading>
+          <Heading tag="h1" variant="large" className="text-[82px] leading-[96px]">
+            Lost in the Sky?
+          </Heading>
+          <Text variant="large" className="text-text">
+            Seems like you&apos;ve ventured into the unknown. Click the button to find your way back (you will
+            be redirected to the homepage in 5 seconds)
+          </Text>
+          <Button variant="primary" className="mt-1 self-center" onClick={() => navigate('/')}>
+            Homepage
           </Button>
-        </Link>
+        </div>
       </div>
     </Layout>
   );
