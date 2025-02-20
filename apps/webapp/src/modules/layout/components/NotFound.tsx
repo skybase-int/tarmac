6538b6f58eed9ab0cb2c8e6ef4589eb29d55681d
iceanddust <iceanddust@proton.me>
2025-02-20 19:18:13 -0300

    Update style of 404 based on designs

--- apps/webapp/src/modules/layout/components/NotFound.tsx
+++ apps/webapp/src/modules/layout/components/NotFound.tsx
@@ -17,20 +17,24 @@ export function NotFound() {
   return (
     <Layout>
       <div className="-mt-16 flex w-full grow flex-col items-center justify-center text-center">
-        <div className="bg-container flex max-w-[650px] flex-col items-center gap-3 rounded-3xl border p-12 bg-blend-overlay backdrop-blur-[50px]">
-          <NoResults />
-          <Heading tag="h3" variant="large" className="text-[32px] leading-9">
+        <div className="bg-container flex max-w-[450px] flex-col items-center gap-3 rounded-3xl border px-12 py-8 bg-blend-overlay backdrop-blur-[50px]">
+          <NoResults className="h-24 w-24" />
+          <Heading tag="h3" variant="medium" className="tracking-[0.0125em]">
             Page not found
           </Heading>
-          <Heading tag="h1" variant="large" className="text-[82px] leading-[96px]">
-            Lost in the Sky?
-          </Heading>
-          <Text variant="large" className="text-text">
-            Seems like you&apos;ve ventured into the unknown. Click the button to find your way back (you will
-            be redirected to the homepage in 5 seconds)
+          <div>
+            <Heading tag="h1" variant="large" className="text-[32px] leading-9 tracking-[0.008em]">
+              Lost in the Sky?
+            </Heading>
+            <Text variant="large" className="text-text/65">
+              Seems like you&apos;ve ventured into the unknown.
+            </Text>
+          </div>
+          <Text variant="large" className="text-text/65 mt-3">
+            Click the button to find your way back (you will be redirected to the homepage in 5 seconds).
           </Text>
-          <Button variant="primary" className="mt-1 self-center" onClick={() => navigate('/')}>
-            Homepage
+          <Button variant="primary" className="mt-6 self-center px-6 py-4" onClick={() => navigate('/')}>
+            Go to Homepage
           </Button>
         </div>
       </div>
