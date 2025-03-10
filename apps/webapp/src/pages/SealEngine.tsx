c5c9c796d4c723a67d596443fe7215da03b241a9
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-10 15:33:19 +0100

    Allow to navigate back to the home page from the seal-engine page

--- apps/webapp/src/pages/SealEngine.tsx
+++ apps/webapp/src/pages/SealEngine.tsx
@@ -1,5 +1,4 @@
 import { Layout } from '@/modules/layout/components/Layout';
-import { SealWidgetPane } from '@/modules/seal/components/SealWidgetPane';
 import { useConfigContext } from '@/modules/config/hooks/useConfigContext';
 import { useCustomConnectModal } from '@/modules/ui/hooks/useCustomConnectModal';
 import { useLingui } from '@lingui/react';
@@ -12,9 +11,23 @@ import { useChainId } from 'wagmi';
 import { ChainModal } from '@/modules/ui/components/ChainModal';
 import { useBreakpointIndex } from '@/modules/ui/hooks/useBreakpointIndex';
 import { BP } from '@/modules/ui/hooks/useBreakpointIndex';
+import { AppContainer } from '@/modules/app/components/AppContainer';
+import { ArrowLeft } from 'lucide-react';
+import { HStack } from '@/modules/layout/components/HStack';
+import { Link } from 'react-router-dom';
+import {
+  SealAction,
+  SealFlow,
+  SealModuleWidget,
+  TxStatus,
+  WidgetStateChangeParams
+} from '@jetstreamgg/widgets';
+import { useCurrentUrnIndex } from '@jetstreamgg/hooks';
+import { useState } from 'react';
 
 export function SealEngine() {
   const { i18n } = useLingui();
+  const { data: currentUrnIndex } = useCurrentUrnIndex();
   const onConnect = useCustomConnectModal();
   const addRecentTransaction = useAddRecentTransaction();
   const { isConnectedAndAcceptedTerms } = useConnectedContext();
@@ -25,8 +38,27 @@ export function SealEngine() {
   const isL2 = isL2ChainId(chainId);
   const { bpi } = useBreakpointIndex();
   const isMobile = bpi < BP.md;
+  const [shouldHideLink, setShouldHideLink] = useState(false);
 
   const referralCode = Number(import.meta.env.VITE_REFERRAL_CODE) || 0; // fallback to 0 if invalid
+
+  let termsLink: any[] = [];
+  try {
+    termsLink = JSON.parse(import.meta.env.VITE_TERMS_LINK);
+  } catch (error) {
+    console.error('Error parsing terms link: ', error);
+  }
+
+  const onSealWidgetStateChange = ({ widgetState, txStatus }: WidgetStateChangeParams) => {
+    const shouldHide =
+      txStatus !== TxStatus.IDLE ||
+      (widgetState.action === SealAction.MULTICALL &&
+        currentUrnIndex !== undefined &&
+        currentUrnIndex > 0n &&
+        (widgetState.flow === SealFlow.OPEN || widgetState.flow === SealFlow.MANAGE));
+
+    setShouldHideLink(shouldHide);
+  };
 
   const sharedProps = {
     onConnect,
@@ -36,27 +68,46 @@ export function SealEngine() {
     onNotification,
     enabled: isConnectedAndAcceptedTerms,
     onExternalLinkClicked,
-    referralCode
+    referralCode,
+    onWidgetStateChange: onSealWidgetStateChange
   };
 
-  // TODO: Work the layout
   return (
     <Layout>
-      <div className="h-full w-[500px] rounded-lg bg-black/50 py-6 pl-6 pr-4">
-        {isL2 ? (
-          <div className="text-center">
-            <Heading>Connect to Mainnet</Heading>
-            <Text className="text-text mt-4">
-              This module is not available on L2s. Please connect to mainnet to use it.
-            </Text>
-            <div className="mt- flex justify-center gap-3">
-              <ChainModal dataTestId="chain-modal-trigger-header" showLabel={!isMobile} variant="widget" />
+      <AppContainer>
+        <div className="h-full w-[500px] rounded-lg py-6 pl-6 pr-4">
+          {!shouldHideLink && (
+            <Link to="/" className="text-textSecondary">
+              <HStack className="mb-3 space-x-2">
+                <ArrowLeft className="self-center" />
+                <Heading tag="h3" variant="small" className="text-textSecondary">
+                  Back to Home
+                </Heading>
+              </HStack>
+            </Link>
+          )}
+          {isL2 ? (
+            <div className="text-center">
+              <Heading variant="large">Connect to Mainnet</Heading>
+              <Text className="text-text mt-8">
+                This module is not available on L2s. Please connect to mainnet to use it.
+              </Text>
+              <div className="mt-10 flex justify-center gap-3">
+                <ChainModal dataTestId="chain-modal-trigger-header" showLabel={!isMobile} variant="widget" />
+              </div>
+              <Text variant="captionSm" className="text-text text-textSecondary mt-2">
+                Select mainnet in the chain selector
+              </Text>
             </div>
-          </div>
-        ) : (
-          <SealWidgetPane {...sharedProps} />
-        )}
-      </div>
+          ) : (
+            <SealModuleWidget
+              {...sharedProps}
+              onWidgetStateChange={onSealWidgetStateChange}
+              termsLink={termsLink[0]}
+            />
+          )}
+        </div>
+      </AppContainer>
     </Layout>
   );
 }
