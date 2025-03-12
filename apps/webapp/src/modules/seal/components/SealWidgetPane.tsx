9c8ac158401b6e7ca6f5688c70187d3c732b5aa5
bitsforpeace <bitsforpeace@proton.me>
2025-03-12 13:36:46 +0100

    Render error if terms missing

--- apps/webapp/src/modules/seal/components/SealWidgetPane.tsx
+++ apps/webapp/src/modules/seal/components/SealWidgetPane.tsx
@@ -7,7 +7,7 @@ import { useSearchParams } from 'react-router-dom';
 import { deleteSearchParams } from '@/modules/utils/deleteSearchParams';
 import { Intent } from '@/lib/enums';
 import { useEffect } from 'react';
-
+import { Error } from '@/modules/layout/components/Error';
 export function SealWidgetPane(sharedProps: SharedProps) {
   let termsLink: any[] = [];
   try {
@@ -99,6 +99,7 @@ export function SealWidgetPane(sharedProps: SharedProps) {
   const hasTermsLink = Array.isArray(termsLink) && termsLink.length > 0;
   if (!hasTermsLink) {
     console.error('No terms link found');
+    return <Error />;
   }
 
   return (
