fe137599a7c86f74fb31a549c3b9b7fe10e14882
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-14 14:22:08 +0100

    Delete seal e2e comments

--- apps/webapp/src/test/e2e/tests/seal.spec.ts
+++ apps/webapp/src/test/e2e/tests/seal.spec.ts
@@ -18,51 +18,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Lock MKR, select rewards, select delegate, and open position', async ({ page }) => {
-  // await page.getByRole('checkbox').click();
-  // await page.getByRole('button', { name: 'Continue' }).click();
-  // await expect(page.getByTestId('supply-first-input-lse-balance')).toHaveText('100 MKR');
-
-  // // fill seal and borrow inputs and click next
-  // await page.getByTestId('supply-first-input-lse').fill('100');
-  // await page.getByTestId('borrow-input-lse').fill('38000');
-
-  // // TODO: check all the params
-  // await expect(page.getByTestId('widget-button')).toBeEnabled();
-  // await page.getByTestId('widget-button').click();
-
-  // // select rewards
-  // await expect(page.getByText('Choose your reward token')).toBeVisible();
-  // await page.getByRole('button', { name: 'USDS' }).click();
-  // await expect(page.getByTestId('widget-button')).toBeEnabled();
-  // await page.getByTestId('widget-button').click();
-
-  // // select delegate
-  // await expect(page.getByText('Choose your delegate')).toBeVisible();
-  // await page.getByRole('button', { name: '0x278' }).click();
-  // await expect(page.getByTestId('widget-button')).toBeEnabled();
-  // await page.getByTestId('widget-button').click();
-
-  // // position summary
-  // await expect(page.getByText('Confirm your position').nth(0)).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Sealing')).toBeVisible();
-  // await expect(page.getByText('100 MKR')).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Borrowing')).toBeVisible();
-  // await expect(page.getByText('38,000 USDS')).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Seal reward')).toBeVisible();
-
-  // // approval
-  // await approveOrPerformAction(page, 'Approve seal amount');
-  // expect(page.getByRole('heading', { name: 'Token access approved' })).toBeVisible();
-
-  // // confirm position
-  // await approveOrPerformAction(page, 'Continue');
-  // expect(page.getByRole('heading', { name: 'Success!' })).toBeVisible();
-  // await expect(
-  //   page.getByText("You've borrowed 38,000 USDS by sealing 100 MKR. Your new position is open.")
-  // ).toBeVisible();
-
   // positions overview
-  // await page.getByRole('button', { name: 'Manage your position(s)' }).click();
   await expect(page.getByText('Position 1')).toBeVisible();
 
   // manage position
@@ -146,52 +102,4 @@ test('Lock MKR, select rewards, select delegate, and open position', async ({ pa
   ).toBeVisible();
   await page.getByRole('button', { name: 'Manage your position(s)' }).click();
   await expect(page.getByText('Position 1')).toBeVisible();
-
-  // // open a new position with SKY
-  // await page.getByRole('button', { name: 'Open a new position' }).click();
-  // await page.getByRole('checkbox').click();
-  // await page.getByRole('button', { name: 'Continue' }).click();
-
-  // // Switch to SKY
-  // await page.getByTestId('view-sky-mkr-button').click();
-
-  // await expect(page.getByTestId('supply-first-input-lse-balance')).toHaveText('100,000,000 SKY');
-
-  // // fill seal and borrow inputs and click next
-  // await page.getByTestId('supply-first-input-lse').fill('100000000');
-  // await page.getByTestId('borrow-input-lse').fill('38000');
-
-  // await expect(page.getByTestId('widget-button').first()).toBeEnabled();
-  // await page.getByTestId('widget-button').first().click();
-
-  // // select rewards
-  // await expect(page.getByText('Choose your reward token')).toBeVisible();
-  // await page.getByRole('button', { name: 'USDS' }).click();
-  // await expect(page.getByTestId('widget-button')).toBeEnabled();
-  // await page.getByTestId('widget-button').click();
-
-  // // select delegate
-  // await expect(page.getByText('Choose your delegate')).toBeVisible();
-  // await page.getByRole('button', { name: '0x278' }).click();
-  // await expect(page.getByTestId('widget-button')).toBeEnabled();
-  // await page.getByTestId('widget-button').click();
-
-  // // position summary
-  // await expect(page.getByText('Confirm your position').nth(0)).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Sealing')).toBeVisible();
-  // await expect(page.getByText('100,000,000 SKY')).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Borrowing')).toBeVisible();
-  // await expect(page.getByText('38,000 USDS')).toBeVisible();
-  // await expect(page.getByTestId('widget-container').getByText('Seal reward')).toBeVisible();
-
-  // // approval
-  // await approveOrPerformAction(page, 'Approve seal amount');
-  // expect(page.getByRole('heading', { name: 'Token access approved' })).toBeVisible();
-
-  // // confirm position
-  // await approveOrPerformAction(page, 'Continue');
-  // expect(page.getByRole('heading', { name: 'Success!' })).toBeVisible();
-  // await expect(
-  //   page.getByText("You've borrowed 38,000 USDS by sealing 100,000,000 SKY. Your new position is open.")
-  // ).toBeVisible();
 });
