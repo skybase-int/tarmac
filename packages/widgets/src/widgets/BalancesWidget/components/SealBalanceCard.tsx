f9ced8f0bbefb92c1c9f6fb03dce45f7d8a498c4
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-19 18:07:16 +0100

    Seal engine migration card in Balances widget

--- packages/widgets/src/widgets/BalancesWidget/components/SealBalanceCard.tsx
+++ packages/widgets/src/widgets/BalancesWidget/components/SealBalanceCard.tsx
@@ -1,22 +1,34 @@
-import { useSealRewardsData, usePrices } from '@jetstreamgg/hooks';
+import { usePrices } from '@jetstreamgg/hooks';
 import { formatBigInt, formatNumber } from '@jetstreamgg/utils';
 import { Text } from '@widgets/shared/components/ui/Typography';
 import { t } from '@lingui/core/macro';
 import { InteractiveStatsCard } from '@widgets/shared/components/ui/card/InteractiveStatsCard';
 import { Skeleton } from '@widgets/components/ui/skeleton';
-import { PopoverRateInfo } from '@widgets/shared/components/ui/PopoverRateInfo';
 import { formatUnits } from 'viem';
 import { CardProps } from './ModulesBalances';
+import { SealBalanceWarningCard } from './SealBalanceWarningCard';
 
-export const SealBalanceCard = ({ url, onExternalLinkClicked, loading, sealBalance }: CardProps) => {
-  const { data: sealRewardsData, isLoading: sealRewardsDataLoading } = useSealRewardsData();
-
+export const SealBalanceCard = ({ url, loading, sealBalance }: CardProps) => {
   const { data: pricesData, isLoading: pricesLoading } = usePrices();
 
-  const sortedSealRewardsData = sealRewardsData ? [...sealRewardsData].sort((a, b) => b.rate - a.rate) : [];
-  const highestSealRewardsRate = sortedSealRewardsData.length > 0 ? sortedSealRewardsData[0].rate : null;
+  const totalSealedValue =
+    sealBalance && pricesData?.MKR
+      ? parseFloat(formatUnits(sealBalance, 18)) * parseFloat(pricesData.MKR.price)
+      : 0;
+  const shouldShowSealWarning = totalSealedValue > 10;
 
-  return (
+  if (totalSealedValue === 0) {
+    return null;
+  }
+
+  return shouldShowSealWarning ? (
+    <SealBalanceWarningCard
+      isLoading={loading || pricesLoading}
+      sealBalance={sealBalance}
+      sealValue={totalSealedValue}
+      url={url}
+    />
+  ) : (
     <InteractiveStatsCard
       title={t`MKR supplied to Seal Engine`}
       tokenSymbol="MKR"
@@ -27,37 +39,19 @@ export const SealBalanceCard = ({ url, onExternalLinkClicked, loading, sealBalan
           <Text>{`${sealBalance ? formatBigInt(sealBalance) : '0'}`}</Text>
         )
       }
-      footer={
-        sealRewardsDataLoading ? (
-          <Skeleton className="h-4 w-20" />
-        ) : highestSealRewardsRate ? (
-          <div className="flex w-fit items-center gap-1.5">
-            <Text variant="small" className="text-bullish leading-4">
-              {`Rates up to: ${highestSealRewardsRate}%`}
-            </Text>
-            <PopoverRateInfo
-              type="srr"
-              onExternalLinkClicked={onExternalLinkClicked}
-              iconClassName="h-[13px] w-[13px]"
-            />
-          </div>
-        ) : (
-          <></>
-        )
-      }
+      footer={pricesLoading ? <Skeleton className="h-4 w-20" /> : <></>}
       footerRightContent={
         loading || pricesLoading ? (
           <Skeleton className="h-[13px] w-20" />
         ) : sealBalance !== undefined && !!pricesData?.MKR ? (
           <Text variant="small" className="text-textSecondary">
             $
-            {formatNumber(parseFloat(formatUnits(sealBalance, 18)) * parseFloat(pricesData.MKR.price), {
+            {formatNumber(totalSealedValue, {
               maxDecimals: 2
             })}
           </Text>
         ) : undefined
       }
-      url={url}
     />
   );
 };
---
+++ packages/widgets/src/widgets/BalancesWidget/components/SealBalanceWarningCard.tsx
@@ -0,0 +1,90 @@
+import { Card, CardContent, CardFooter } from '@widgets/components/ui/card';
+import { Text } from '@widgets/shared/components/ui/Typography';
+import { TokenIcon } from '@widgets/shared/components/ui/token/TokenIcon';
+import { Skeleton } from '@widgets/components/ui/skeleton';
+import { formatBigInt, formatNumber } from '@jetstreamgg/utils';
+import { VStack } from '@widgets/shared/components/ui/layout/VStack';
+import { HStack } from '@widgets/shared/components/ui/layout/HStack';
+import { Warning } from '@widgets/shared/components/icons/Warning';
+import { Button } from '@widgets/components/ui/button';
+import { useNavigate } from 'react-router-dom';
+import { Trans } from '@lingui/react/macro';
+
+export const SealBalanceWarningCard = ({
+  url,
+  isLoading,
+  sealBalance,
+  sealValue
+}: {
+  url?: string;
+  isLoading?: boolean;
+  sealBalance?: bigint;
+  sealValue?: number;
+}): React.ReactElement => {
+  const navigate = useNavigate();
+  const isLargeAmount = sealValue && sealValue > 1000000;
+
+  return (
+    <Card className="bg-red-400/10 p-4 bg-blend-overlay lg:p-5">
+      <div className="flex items-start gap-2">
+        <TokenIcon className="h-8 w-8" token={{ symbol: 'MKR', name: 'MKR' }} chainId={1} />{' '}
+        <div className="grow">
+          <CardContent className="flex flex-col items-start">
+            <div className="flex w-full justify-between">
+              <Text>
+                <Trans>MKR supplied to Seal Engine</Trans>
+              </Text>
+              {isLoading ? (
+                <Skeleton className="w-32" />
+              ) : (
+                <VStack gap={0} className="text-right">
+                  <Text>{`${sealBalance ? formatBigInt(sealBalance) : '0'}`}</Text>
+                </VStack>
+              )}
+            </div>
+            <div className="flex w-full justify-between">
+              <HStack className="items-center" gap={1}>
+                <Warning boxSize={14} viewBox="0 0 16 16" />
+                <Text variant="captionSm" className="text-textSecondary text-error">
+                  <Trans>Seal Engine is deprecated</Trans>
+                </Text>
+              </HStack>
+              {isLoading ? (
+                <Skeleton className="w-32" />
+              ) : (
+                <VStack gap={0} className="text-right">
+                  {sealValue !== undefined ? (
+                    <Text variant="small" className="text-textSecondary">
+                      $
+                      {formatNumber(sealValue, {
+                        maxDecimals: isLargeAmount ? 0 : 2
+                      })}
+                    </Text>
+                  ) : undefined}
+                </VStack>
+              )}
+            </div>
+          </CardContent>
+          {url && (
+            <CardFooter>
+              <div className="flex w-full justify-end pt-4">
+                <Button
+                  className="h-6 bg-[#7c53c5] outline outline-1 outline-offset-[-1px] outline-white/20 hover:bg-[#9a75e0] focus:bg-[#9a75e0] active:bg-[#9a75e0]/80"
+                  variant="pill"
+                  size="sm"
+                  onClick={() => {
+                    navigate(url);
+                  }}
+                >
+                  <Text variant="captionSm">
+                    <Trans>Migrate to Activation Engine</Trans>
+                  </Text>
+                </Button>
+              </div>
+            </CardFooter>
+          )}
+        </div>
+      </div>
+    </Card>
+  );
+};
