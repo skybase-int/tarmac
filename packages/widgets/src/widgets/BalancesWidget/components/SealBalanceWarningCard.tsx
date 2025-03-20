02156bd074cae95a66ddfc5244809c0f51df08d5
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-20 15:45:40 +0100

    Update Seal warning card in balances widget

--- packages/widgets/src/widgets/BalancesWidget/components/SealBalanceWarningCard.tsx
+++ packages/widgets/src/widgets/BalancesWidget/components/SealBalanceWarningCard.tsx
@@ -6,9 +6,9 @@ import { formatBigInt, formatNumber } from '@jetstreamgg/utils';
 import { VStack } from '@widgets/shared/components/ui/layout/VStack';
 import { HStack } from '@widgets/shared/components/ui/layout/HStack';
 import { Warning } from '@widgets/shared/components/icons/Warning';
-import { Button } from '@widgets/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Trans } from '@lingui/react/macro';
+import { ArrowRight } from 'lucide-react';
 
 export const SealBalanceWarningCard = ({
   url,
@@ -21,70 +21,70 @@ export const SealBalanceWarningCard = ({
   sealBalance?: bigint;
   sealValue?: number;
 }): React.ReactElement => {
-  const navigate = useNavigate();
   const isLargeAmount = sealValue && sealValue > 1000000;
 
   return (
-    <Card className="bg-red-400/10 p-4 bg-blend-overlay lg:p-5">
-      <div className="flex items-start gap-2">
-        <TokenIcon className="h-8 w-8" token={{ symbol: 'MKR', name: 'MKR' }} chainId={1} />{' '}
-        <div className="grow">
-          <CardContent className="flex flex-col items-start">
-            <div className="flex w-full justify-between">
-              <Text>
-                <Trans>MKR supplied to Seal Engine</Trans>
-              </Text>
-              {isLoading ? (
-                <Skeleton className="w-32" />
-              ) : (
-                <VStack gap={0} className="text-right">
-                  <Text>{`${sealBalance ? formatBigInt(sealBalance) : '0'}`}</Text>
-                </VStack>
-              )}
-            </div>
-            <div className="flex w-full justify-between">
-              <HStack className="items-center" gap={1}>
-                <Warning boxSize={14} viewBox="0 0 16 16" />
-                <Text variant="captionSm" className="text-error">
-                  <Trans>Seal Engine is deprecated</Trans>
+    <Link to={url ?? ''}>
+      <Card className="group/warning-seal-card bg-red-400/10 p-4 bg-blend-overlay hover:bg-red-400/25 lg:p-5">
+        <div className="flex items-start gap-2">
+          <TokenIcon className="h-8 w-8" token={{ symbol: 'MKR', name: 'MKR' }} chainId={1} />{' '}
+          <div className="grow">
+            <CardContent className="flex flex-col items-start">
+              <div className="flex w-full justify-between">
+                <Text>
+                  <Trans>MKR supplied to Seal Engine</Trans>
                 </Text>
-              </HStack>
-              {isLoading ? (
-                <Skeleton className="w-32" />
-              ) : (
-                <VStack gap={0} className="text-right">
-                  {sealValue !== undefined ? (
-                    <Text variant="small" className="text-textSecondary">
-                      $
-                      {formatNumber(sealValue, {
-                        maxDecimals: isLargeAmount ? 0 : 2
-                      })}
-                    </Text>
-                  ) : undefined}
-                </VStack>
-              )}
-            </div>
-          </CardContent>
-          {url && (
-            <CardFooter>
-              <div className="flex w-full justify-end pt-4">
-                <Button
-                  className="h-6 bg-[#7c53c5] outline outline-1 outline-offset-[-1px] outline-white/20 hover:bg-[#9a75e0] focus:bg-[#9a75e0] active:bg-[#9a75e0]/80"
-                  variant="pill"
-                  size="sm"
-                  onClick={() => {
-                    navigate(url);
-                  }}
-                >
-                  <Text variant="captionSm">
-                    <Trans>Migrate to Activation Engine</Trans>
-                  </Text>
-                </Button>
+                {isLoading ? (
+                  <Skeleton className="w-32" />
+                ) : (
+                  <VStack gap={0} className="text-right">
+                    <Text>{`${sealBalance ? formatBigInt(sealBalance) : '0'}`}</Text>
+                  </VStack>
+                )}
               </div>
-            </CardFooter>
-          )}
+              <div className="flex w-full justify-between">
+                <HStack className="items-center" gap={1}>
+                  <Warning boxSize={14} viewBox="0 0 16 16" />
+                  <Text variant="captionSm" className="text-error">
+                    <Trans>Seal Engine is deprecated</Trans>
+                  </Text>
+                </HStack>
+                {isLoading ? (
+                  <Skeleton className="w-32" />
+                ) : (
+                  <VStack gap={0} className="text-right">
+                    {sealValue !== undefined ? (
+                      <Text variant="small" className="text-textSecondary">
+                        $
+                        {formatNumber(sealValue, {
+                          maxDecimals: isLargeAmount ? 0 : 2
+                        })}
+                      </Text>
+                    ) : undefined}
+                  </VStack>
+                )}
+              </div>
+            </CardContent>
+          </div>
         </div>
-      </div>
-    </Card>
+        {url && (
+          <CardFooter>
+            <div className="flex w-full justify-start pt-4">
+              <div className="flex grow items-center gap-2">
+                <Text variant="captionSm">
+                  <Trans>Migrate your position(s) to the Activation Engine</Trans>
+                </Text>
+                <div className="mt-1 h-4 w-4">
+                  <ArrowRight
+                    size={16}
+                    className="opacity-0 transition-opacity group-hover/warning-seal-card:opacity-100"
+                  />
+                </div>
+              </div>
+            </div>
+          </CardFooter>
+        )}
+      </Card>
+    </Link>
   );
 };
