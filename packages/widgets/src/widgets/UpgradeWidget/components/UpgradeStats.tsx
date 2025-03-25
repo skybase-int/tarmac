d710b2238fc8f943c6a721c1e6007ad1c43421ac
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-25 16:02:27 +0100

    Show upgrade stats both for dai and mkr

--- packages/widgets/src/widgets/UpgradeWidget/components/UpgradeStats.tsx
+++ packages/widgets/src/widgets/UpgradeWidget/components/UpgradeStats.tsx
@@ -5,13 +5,7 @@ import { formatBigInt } from '@jetstreamgg/utils';
 import { t } from '@lingui/core/macro';
 import { useRef } from 'react';
 
-export const UpgradeStats = ({
-  shouldShowDai,
-  shouldShowMkr
-}: {
-  shouldShowDai: boolean;
-  shouldShowMkr: boolean;
-}) => {
+export const UpgradeStats = () => {
   // TODO handle loading and error states
   const { data } = useUpgradeTotals();
   const ref = useRef<HTMLDivElement>(null);
@@ -20,36 +14,33 @@ export const UpgradeStats = ({
 
   return (
     <div ref={ref} className="my-4 flex justify-between gap-3">
-      {shouldShowDai && (
-        <StatsCard
-          title={t`Total DAI migrated`}
-          content={
-            <TokenIconWithBalance
-              className="mt-1"
-              token={{ symbol: 'DAI', name: 'dai' }}
-              balance={
-                data?.totalDaiUpgraded ? formatBigInt(data?.totalDaiUpgraded, { compact: isCompact }) : '0'
-              }
-              compact={isCompact}
-            />
-          }
-        ></StatsCard>
-      )}
-      {shouldShowMkr && (
-        <StatsCard
-          title={t`Total MKR migrated`}
-          content={
-            <TokenIconWithBalance
-              className="mt-1"
-              token={{ symbol: 'MKR', name: 'mkr' }}
-              balance={
-                data?.totalMkrUpgraded ? formatBigInt(data?.totalMkrUpgraded, { compact: isCompact }) : '0'
-              }
-              compact={isCompact}
-            />
-          }
-        ></StatsCard>
-      )}
+      <StatsCard
+        title={t`Total DAI migrated`}
+        content={
+          <TokenIconWithBalance
+            className="mt-1"
+            token={{ symbol: 'DAI', name: 'dai' }}
+            balance={
+              data?.totalDaiUpgraded ? formatBigInt(data?.totalDaiUpgraded, { compact: isCompact }) : '0'
+            }
+            compact={isCompact}
+          />
+        }
+      ></StatsCard>
+
+      <StatsCard
+        title={t`Total MKR migrated`}
+        content={
+          <TokenIconWithBalance
+            className="mt-1"
+            token={{ symbol: 'MKR', name: 'mkr' }}
+            balance={
+              data?.totalMkrUpgraded ? formatBigInt(data?.totalMkrUpgraded, { compact: isCompact }) : '0'
+            }
+            compact={isCompact}
+          />
+        }
+      ></StatsCard>
     </div>
   );
 };
