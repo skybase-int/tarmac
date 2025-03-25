8b069f43bda6dc6c8df91d1984d4913a29a28cd1
Code with the Flow <codewiththeflowxyz@proton.me>
2025-03-25 15:08:51 +0100

    Update linked actions tests and labels

--- apps/webapp/src/test/e2e/tests/la-u-r.spec.ts
+++ apps/webapp/src/test/e2e/tests/la-u-r.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '../fixtures.ts';
 import { approveOrPerformAction } from '../utils/approveOrPerformAction.ts';
 import { connectMockWalletAndAcceptTerms } from '../utils/connectMockWalletAndAcceptTerms.ts';
 
-test('Linked Action - Upgrade DAI then get reward', async ({ page }) => {
+test('Linked Action - Migrate DAI then get reward', async ({ page }) => {
   await page.goto(
     '?widget=upgrade&input_amount=100&linked_action=rewards&reward=0x0650CAF159C5A49f711e8169D4336ECB9b950275'
   );
@@ -10,7 +10,7 @@ test('Linked Action - Upgrade DAI then get reward', async ({ page }) => {
   const arrowStepIndicators = page.locator('[data-testid="arrow-step-indicator"]');
   await expect(arrowStepIndicators).toHaveCount(2);
 
-  await approveOrPerformAction(page, 'Upgrade');
+  await approveOrPerformAction(page, 'Migrate');
 
   await page.getByRole('button', { name: 'Go to Rewards' }).first().click();
 
