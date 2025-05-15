5c8bd94b51d650d66bb81b8f6009f31ccf4eea23
incline <inclinedmind@proton.me>
2025-05-15 15:38:39 -0700

    don't convert mkr to sky in stake

--- packages/widgets/src/widgets/StakeModuleWidget/components/PositionDetailsAccordion.tsx
+++ packages/widgets/src/widgets/StakeModuleWidget/components/PositionDetailsAccordion.tsx
@@ -7,7 +7,7 @@ import {
 import { positionAnimations } from '@widgets/shared/animation/presets';
 import { TextWithTooltip } from '@widgets/shared/components/ui/tooltip/TextWithTooltip';
 import { Text } from '@widgets/shared/components/ui/Typography';
-import { captitalizeFirstLetter, formatBigInt, formatPercent, math } from '@jetstreamgg/utils';
+import { captitalizeFirstLetter, formatBigInt, formatPercent } from '@jetstreamgg/utils';
 import { motion } from 'framer-motion';
 import { getRiskTextColor } from '../lib/utils';
 import { RiskLevel, SupportedCollateralTypes, useCollateralData } from '@jetstreamgg/hooks';
@@ -113,17 +113,13 @@ export function PositionDetailAccordion({
                 gap={1}
                 iconClassName="text-textSecondary"
               />
-              <Text className="text-right text-sm">
-                ${formatBigInt(math.calculateMKRtoSKYPrice(liquidationPrice))}
-              </Text>
+              <Text className="text-right text-sm">${formatBigInt(liquidationPrice)}</Text>
             </motion.div>
           )}
           {!!delayedPrice && delayedPrice > 0n && (
             <motion.div className="flex justify-between" variants={positionAnimations}>
               <Text className="text-textSecondary text-sm font-normal leading-4">Current SKY price</Text>
-              <Text className="text-right text-sm">
-                ${formatBigInt(math.calculateMKRtoSKYPrice(delayedPrice))}
-              </Text>
+              <Text className="text-right text-sm">${formatBigInt(delayedPrice)}</Text>
             </motion.div>
           )}
           {!!riskLevel && (
