15462698416ff8a793affe8f74a0cd0db6d3b295
Santiago Botto <santiago@techops.services>
2025-05-18 13:23:39 -0300

    TECH-4973 - Add Cookie3 to prod

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -28,6 +28,7 @@ export default ({ mode }: { mode: modeEnum }) => {
     default-src 'self';
     script-src 'self'
       'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='
+      https://cdn.markfi.xyz
       https://static.cloudflareinsights.com
       https://challenges.cloudflare.com;
     style-src 'self' 'unsafe-inline';
@@ -67,6 +68,7 @@ export default ({ mode }: { mode: modeEnum }) => {
       wss://www.walletlink.org
       https://explorer-api.walletconnect.com/
       https://enhanced-provider.rainbow.me
+      https://a.markfi.xyz
       cloudflareinsights.com;
     frame-src 'self'
       https://verify.walletconnect.com
