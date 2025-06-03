ecda341c16457105a167940afd19089bf7067be3
generatorpoint <generatorpoint@proton.me>
2025-06-03 15:21:19 -0600

    add log

--- apps/webapp/src/test/e2e/utils/getRpcUrlFromFile.ts
+++ apps/webapp/src/test/e2e/utils/getRpcUrlFromFile.ts
@@ -18,6 +18,7 @@ export const getRpcUrlFromFile = async (network: NetworkName): Promise<string> =
 
   const networkData = data.find((item: TestnetData) => item.NETWORK === chainName);
   if (!networkData) {
+    console.error(`ChainName: ${chainName}. Data to review: ${data}`);
     throw new Error(`No RPC URL found for network ${network}`);
   }
 
