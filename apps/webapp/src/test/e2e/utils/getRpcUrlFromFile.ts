9842dc910200bcb330081c558f28f00fa1a6cc12
generatorpoint <generatorpoint@proton.me>
2025-06-03 15:13:58 -0600

    fix getter file

--- apps/webapp/src/test/e2e/utils/getRpcUrlFromFile.ts
+++ apps/webapp/src/test/e2e/utils/getRpcUrlFromFile.ts
@@ -1,6 +1,8 @@
 import { readFile } from 'fs/promises';
 import { NetworkName } from './constants';
 
+type TestnetData = { NETWORK: string; TENDERLY_TESTNET_ID: string; TENDERLY_RPC_URL: string };
+
 export const getRpcUrlFromFile = async (network: NetworkName): Promise<string> => {
   const file = await readFile('../../tenderlyTestnetData.json', 'utf-8');
   const data = JSON.parse(file);
@@ -14,9 +16,10 @@ export const getRpcUrlFromFile = async (network: NetworkName): Promise<string> =
     [NetworkName.unichain]: 'unichain'
   }[network];
 
-  if (!data[chainName]) {
+  const networkData = data.find((item: TestnetData) => item.NETWORK === chainName);
+  if (!networkData) {
     throw new Error(`No RPC URL found for network ${network}`);
   }
 
-  return data[chainName].TENDERLY_RPC_URL;
+  return networkData.TENDERLY_RPC_URL;
 };
