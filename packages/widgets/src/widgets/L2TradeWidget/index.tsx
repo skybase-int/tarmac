82ad00f8fb48904d4b7c3078fbe4a8465ede111f
iceanddust <iceanddust@proton.me>
2025-06-06 17:08:56 -0300

    Wait for debounce on a single input

--- packages/widgets/src/widgets/L2TradeWidget/index.tsx
+++ packages/widgets/src/widgets/L2TradeWidget/index.tsx
@@ -515,7 +515,6 @@ function TradeWidgetWrapped({
           // Only update if the amount has actually changed
           if (newAmount !== originAmount) {
             timeoutId = setTimeout(() => {
-              console.log('Updating side');
               // Batch all state updates together
               setOriginAmount(newAmount);
               // setTargetAmount(0n);
@@ -723,7 +722,9 @@ function TradeWidgetWrapped({
   const prepareError = approvePrepareError;
 
   const isAmountWaitingForDebounce =
-    debouncedOriginAmount !== originAmount || debouncedTargetAmount !== targetAmount;
+    lastUpdated === TradeSide.IN
+      ? debouncedOriginAmount !== originAmount
+      : debouncedTargetAmount !== targetAmount;
 
   const approveDisabled =
     [TxStatus.INITIALIZED, TxStatus.LOADING].includes(txStatus) ||
