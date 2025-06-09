93c21e5e140a1eccc59be53beeb439ce14b087b7
generatorpoint <generatorpoint@proton.me>
2025-06-09 16:36:17 -0600

    add some debugging

--- scripts/forkVnet.ts
+++ scripts/forkVnet.ts
@@ -24,7 +24,7 @@ const UNICHAIN_CONFIG = {
 const forkVnets = async chainType => {
   const currentTime = Date.now();
 
-  const chainsToFork = chainType ?? ['mainnet', 'base', 'arbitrum', 'optimism', 'unichain'];
+  const chainsToFork = chainType ?? ['mainnet', 'base', 'arbitrum' /*'optimism', 'unichain'*/]; // Re-enable when we add tests for these chains
 
   const responses = await Promise.all(
     //@ts-expect-error script doesn't work with TS
