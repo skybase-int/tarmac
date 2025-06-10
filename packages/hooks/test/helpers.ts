a91a91f59823f66ae6dc670d4dee2bba73e55618
iceanddust <iceanddust@proton.me>
2025-06-10 19:44:18 -0300

    Add tests for l2 psm hooks

--- packages/hooks/test/helpers.ts
+++ packages/hooks/test/helpers.ts
@@ -1,10 +1,12 @@
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
 import { WriteHook } from '../src';
 import { renderHook, waitFor } from '@testing-library/react';
 import { WagmiWrapper } from './WagmiWrapper';
+import { BatchWriteHook } from '../src/hooks';
+import { TEST_WALLET_ADDRESS } from './constants';
 
 export const waitForPreparedExecuteAndMine = async (
-  result: { current: WriteHook },
+  result: { current: WriteHook | BatchWriteHook },
   loadingTimeout: number = 3000
 ) => {
   await waitFor(
@@ -41,3 +43,33 @@ export const getUrnAddress = async (urnIndex: bigint, useUrnAddress: any) => {
   );
   return resultUrnAddress.current.data as `0x${string}`;
 };
+
+export const getUseCapabilitiesMockedResponse = (chainId: number) =>
+  ({
+    data: { [chainId]: { atomic: { status: 'supported' } } },
+    isLoading: false,
+    error: null,
+    refetch: vi.fn(),
+    isError: false,
+    isPending: false,
+    isLoadingError: false,
+    isRefetchError: false,
+    isSuccess: true,
+    isPlaceholderData: false,
+    status: 'success',
+    dataUpdatedAt: Date.now(),
+    errorUpdatedAt: Date.now(),
+    failureCount: 0,
+    failureReason: null,
+    errorUpdateCount: 0,
+    isFetched: true,
+    isFetchedAfterMount: true,
+    isFetching: false,
+    isRefetching: false,
+    isInitialLoading: false,
+    isStale: false,
+    isPaused: false,
+    fetchStatus: 'idle',
+    promise: Promise.resolve(),
+    queryKey: ['capabilities', { account: TEST_WALLET_ADDRESS }]
+  }) as const;
