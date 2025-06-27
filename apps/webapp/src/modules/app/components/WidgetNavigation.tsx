b7c11556373a9a19fcfee0eb8509757147c1e09d
codewiththeflowxyz <codewiththeflowxyz@proton.me>
2025-06-27 17:28:02 +0200

    Adjust tabs padding

--- apps/webapp/src/modules/app/components/WidgetNavigation.tsx
+++ apps/webapp/src/modules/app/components/WidgetNavigation.tsx
@@ -172,11 +172,11 @@ export function WidgetNavigation({
       >
         <motion.div layout transition={{ layout: { duration: 0 } }} className="md:flex md:w-full md:flex-row">
           {/* Desktop vertical tabs, hidden on mobile */}
-          <div className="border-r-1 h-full">
+          <div className="border-r-1 h-full justify-center">
             <TabsList
               className={cn(
-                'sticky top-0 z-20 flex w-full justify-around rounded-none rounded-t-3xl border-b p-3 backdrop-blur-2xl',
-                'md:scrollbar-thin md:static md:mt-3 md:h-fit md:max-h-[calc(100vh-120px)] md:w-auto md:flex-col md:justify-start md:gap-2 md:self-start md:overflow-y-auto md:rounded-none md:border-0 md:bg-transparent md:p-0 md:pr-2 md:backdrop-filter-none',
+                'sticky top-0 z-20 flex w-full justify-around rounded-none rounded-t-3xl border-b backdrop-blur-2xl',
+                'md:scrollbar-thin md:static md:mt-3 md:h-fit md:max-h-[calc(100vh-120px)] md:w-auto md:flex-col md:justify-start md:gap-2 md:self-start md:overflow-y-auto md:rounded-none md:border-0 md:bg-transparent md:p-0 md:backdrop-filter-none',
                 hideTabs && 'hidden',
                 isMobile && 'hidden' // Hide the horizontal tabs on mobile when using Sheet
               )}
