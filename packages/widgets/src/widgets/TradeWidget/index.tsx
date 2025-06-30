ef80d72222c128f4b58b8d2b39489f022cae3537
bitsforpeace <bitsforpeace@proton.me>
2025-06-30 09:32:51 +0200

    Update TradeWidget to conditionally display back button after successful trade based on target token

--- packages/widgets/src/widgets/TradeWidget/index.tsx
+++ packages/widgets/src/widgets/TradeWidget/index.tsx
@@ -1174,7 +1174,10 @@ function TradeWidgetWrapped({
     txStatus === TxStatus.ERROR ||
     (widgetState.action === TradeAction.TRADE && widgetState.screen === TradeScreen.REVIEW) ||
     (widgetState.action === TradeAction.APPROVE && widgetState.screen === TradeScreen.REVIEW) ||
-    (widgetState.action === TradeAction.TRADE && txStatus === TxStatus.SUCCESS) ||
+    // After a successful trade, show the back button, as long as target token is not ETH
+    (targetToken?.symbol !== 'ETH' &&
+      widgetState.action === TradeAction.TRADE &&
+      txStatus === TxStatus.SUCCESS) ||
     // After a successful approve transaction, show the back button
     (txStatus === TxStatus.SUCCESS &&
       widgetState.action === TradeAction.APPROVE &&
