e9c92d2c98f4a1276d1420c889ca36fe0c3867d9
iceanddust <iceanddust@proton.me>
2025-07-23 17:28:22 -0300

    Update logo sizes and positions for responsive design

--- apps/webapp/src/modules/ui/components/HighlightLogo.tsx
+++ apps/webapp/src/modules/ui/components/HighlightLogo.tsx
@@ -6,35 +6,35 @@ const logoMetadata = [
     src: '/images/upgrade_icon_large.svg',
     alt: 'Upgrade logo',
     position: '-bottom-2 -right-5',
-    size: 'w-48'
+    size: 'w-40 2xl:w-48'
   },
   {
     name: 'trade',
     src: '/images/trade_icon_large.svg',
     alt: 'Trade logo',
     position: '-bottom-12 -right-12',
-    size: 'w-64'
+    size: 'w-56 2xl:w-64'
   },
   {
     name: 'savings',
     src: '/images/savings_icon_large.svg',
     alt: 'Savings logo',
     position: '-bottom-5 -right-8',
-    size: 'w-52'
+    size: 'w-50 2xl:w-52'
   },
   {
     name: 'rewards',
     src: '/images/rewards_icon_large.svg',
     alt: 'Rewards logo',
     position: '-bottom-5 -right-12',
-    size: 'w-56'
+    size: 'w-50 2xl:w-56'
   },
   {
     name: 'staking',
     src: '/images/staking_engine_icon_large.svg',
     alt: 'Staking Engine logo',
-    position: '-bottom-5 -right-5',
-    size: 'w-56'
+    position: '-bottom-8 -right-5',
+    size: 'w-50 2xl:w-56'
   }
 ] as const;
 
