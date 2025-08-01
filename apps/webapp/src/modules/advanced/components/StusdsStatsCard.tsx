5cc7e461c8a3cb5984213cd04fbbf352c6300492
iceanddust <iceanddust@proton.me>
2025-08-01 11:44:47 -0300

    Fix color transition

--- apps/webapp/src/modules/advanced/components/StusdsStatsCard.tsx
+++ apps/webapp/src/modules/advanced/components/StusdsStatsCard.tsx
@@ -30,7 +30,7 @@ export const StusdsStatsCard = ({ onClick, disabled = false }: StusdsStatsCardPr
 
   return (
     <Card
-      className={`h-full transition-all lg:p-5 ${onClick && !disabled ? 'hover:bg-radial-(--gradient-position) hover:from-primary-start/100 hover:to-primary-end/100 cursor-pointer' : ''} ${disabled ? 'opacity-50' : ''}`}
+      className={`bg-radial-(--gradient-position) from-card to-card h-full transition lg:p-5 ${onClick && !disabled ? 'hover:from-primary-start/100 hover:to-primary-end/100 cursor-pointer' : ''} ${disabled ? 'opacity-50' : ''}`}
       onClick={disabled ? undefined : onClick}
     >
       <CardHeader className="flex flex-row items-center justify-between space-y-0">
