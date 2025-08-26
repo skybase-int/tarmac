d8a97f0e3c62afb8ea238187490a41d38f0dd665
iceanddust <iceanddust@proton.me>
2025-08-26 18:03:14 -0300

    Add Cow API endpoint and relayer address for new chains

--- packages/hooks/src/trade/constants.ts
+++ packages/hooks/src/trade/constants.ts
@@ -1,4 +1,4 @@
-import { mainnet } from 'wagmi/chains';
+import { arbitrum, base, mainnet } from 'wagmi/chains';
 import createClient from 'openapi-fetch';
 import { paths } from './cowApiSchema';
 
@@ -8,7 +8,9 @@ export enum TradeSide {
 }
 
 const COW_API_ENDPOINT = {
-  [mainnet.id]: 'https://api.cow.fi/mainnet'
+  [mainnet.id]: 'https://api.cow.fi/mainnet',
+  [base.id]: 'https://api.cow.fi/base',
+  [arbitrum.id]: 'https://api.cow.fi/arbitrum_one'
 } as const;
 
 export enum OrderQuoteSideKind {
@@ -52,9 +54,13 @@ export const ETH_FLOW_QUOTE_PARAMS = {
 } as const;
 
 export const gpv2VaultRelayerAddress = {
-  [mainnet.id]: '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110'
+  [mainnet.id]: '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110',
+  [base.id]: '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110',
+  [arbitrum.id]: '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110'
 } as const;
 
 export const cowApiClient = {
-  [mainnet.id]: createClient<paths>({ baseUrl: COW_API_ENDPOINT[mainnet.id] })
+  [mainnet.id]: createClient<paths>({ baseUrl: COW_API_ENDPOINT[mainnet.id] }),
+  [base.id]: createClient<paths>({ baseUrl: COW_API_ENDPOINT[base.id] }),
+  [arbitrum.id]: createClient<paths>({ baseUrl: COW_API_ENDPOINT[arbitrum.id] })
 } as const;
