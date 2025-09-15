c7d12f8ef9e8dce879a7fbe64f867437e7664d3f
incline <inclinedmind@proton.me>
2025-09-15 11:49:53 -0700

    add og tags

--- apps/webapp/src/modules/layout/components/Layout.tsx
+++ apps/webapp/src/modules/layout/components/Layout.tsx
@@ -36,6 +36,20 @@ export function Layout({
       <meta name="description" content={descriptionContent} />
       <link rel="icon" href={siteConfig.favicon} />
 
+      {/* Open Graph Tags */}
+      <meta property="og:title" content={titleContent} />
+      <meta property="og:description" content={descriptionContent} />
+      <meta property="og:image" content={`${window.location.origin}/images/social_media_image.png`} />
+      <meta property="og:url" content={window.location.href} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={siteConfig.name} />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={titleContent} />
+      <meta name="twitter:description" content={descriptionContent} />
+      <meta name="twitter:image" content={`${window.location.origin}/images/social_media_image.png`} />
+
       <VStack
         className={
           'bg-app-background flex max-h-svh min-h-svh max-w-full items-center overflow-auto bg-cover bg-center bg-no-repeat md:max-h-screen md:min-h-screen md:p-4 md:pb-2'
