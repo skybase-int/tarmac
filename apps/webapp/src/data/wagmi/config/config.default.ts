e3f7d6c6eea6167ceac3da6deac550f2e5fb9530
iceanddust <iceanddust@proton.me>
2025-09-25 15:43:57 -0300

    Import latest version of Porto from Wagmi

--- apps/webapp/src/data/wagmi/config/config.default.ts
+++ apps/webapp/src/data/wagmi/config/config.default.ts
@@ -1,9 +1,8 @@
 import { createConfig, createStorage, http, noopStorage } from 'wagmi';
 import { mainnet, base, sepolia, arbitrum, optimism, unichain } from 'wagmi/chains';
-import { metaMask, safe, walletConnect, coinbaseWallet, baseAccount } from 'wagmi/connectors';
+import { metaMask, safe, walletConnect, coinbaseWallet, baseAccount, porto } from 'wagmi/connectors';
 import { TENDERLY_CHAIN_ID, TENDERLY_RPC_URL } from './testTenderlyChain';
 import { isTestnetId } from '@jetstreamgg/sky-utils';
-import { porto } from 'porto/wagmi';
 
 export const tenderly = {
   ...mainnet,
