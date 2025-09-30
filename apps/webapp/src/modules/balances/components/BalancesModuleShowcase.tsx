ebd396a83441b2e8e11675bcc1620ba1d938cb31
codewiththeflowxyz <codewiththeflowxyz@proton.me>
2025-09-30 14:28:35 +0200

    Remove expert modules feature flag

--- apps/webapp/src/modules/balances/components/BalancesModuleShowcase.tsx
+++ apps/webapp/src/modules/balances/components/BalancesModuleShowcase.tsx
@@ -9,7 +9,6 @@ import { UpgradeCard } from './modules/UpgradeCard';
 import { StakingRewardsCard } from './modules/StakingRewardsCard';
 import { ExpertCard } from './modules/ExpertCard';
 import { BP, useBreakpointIndex } from '@/modules/ui/hooks/useBreakpointIndex';
-import { isExpertModulesEnabled } from '@/lib/feature-flags';
 
 type CarouselCard = {
   id: string;
@@ -68,7 +67,6 @@ export function BalancesModuleShowcase() {
     const filtered = allCards.filter(card => {
       if (card.hideInRestrictedBuild && isRestrictedBuild) return false;
       if (card.hideInMiCaRestricted && isRestrictedMiCa) return false;
-      if (card.id === 'expert' && !isExpertModulesEnabled()) return false;
       return true;
     });
 
