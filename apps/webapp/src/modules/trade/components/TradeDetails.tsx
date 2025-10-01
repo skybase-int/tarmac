54a752e9f5abfad3984d944dcd2fd3fee65577a7
bitsforpeace <bitsforpeace@proton.me>
2025-10-01 09:51:48 +0200

    Add AboutTokensCarousel component and integrate it into TradeDetails. Refactor carousel implementation for better modularity and maintainability.

--- apps/webapp/src/modules/trade/components/TradeDetails.tsx
+++ apps/webapp/src/modules/trade/components/TradeDetails.tsx
@@ -1,32 +1,22 @@
 import { t } from '@lingui/core/macro';
-import { Carousel, CarouselContent, CarouselItem, CarouselControls } from '@/components/ui/carousel';
-import Autoplay from 'embla-carousel-autoplay';
-import Fade from 'embla-carousel-fade';
 import { TradeHistory } from './TradeHistory';
 import { TradeFaq } from './TradeFaq';
 import { DetailSectionWrapper } from '@/modules/ui/components/DetailSectionWrapper';
 import { DetailSectionRow } from '@/modules/ui/components/DetailSectionRow';
 import { DetailSection } from '@/modules/ui/components/DetailSection';
-import { AboutUsds } from '@/modules/ui/components/AboutUsds';
 import { ActionsShowcase } from '@/modules/ui/components/ActionsShowcase';
 import { useConnectedContext } from '@/modules/ui/context/ConnectedContext';
 import { IntentMapping } from '@/lib/constants';
 import { useConfigContext } from '@/modules/config/hooks/useConfigContext';
 import { useUserSuggestedActions } from '@/modules/ui/hooks/useUserSuggestedActions';
 import { filterActionsByIntent } from '@/lib/utils';
-import { AboutSky } from '@/modules/ui/components/AboutSky';
-import { AboutSpk } from '@/modules/ui/components/AboutSpk';
-import { AboutSUsds } from '@/modules/ui/components/AboutSUsds';
-import { BP, useBreakpointIndex } from '@/modules/ui/hooks/useBreakpointIndex';
+import { AboutTokensCarousel } from './AboutTokensCarousel';
 
 export function TradeDetails(): React.ReactElement {
   const { isConnectedAndAcceptedTerms } = useConnectedContext();
   const { linkedActionConfig } = useConfigContext();
   const { data: actionData } = useUserSuggestedActions();
   const widget = IntentMapping.TRADE_INTENT;
-
-  const { bpi } = useBreakpointIndex();
-  const isMobileOrTablet = bpi < BP['2xl'];
 
   return (
     <DetailSectionWrapper>
@@ -47,45 +37,7 @@ export function TradeDetails(): React.ReactElement {
         </DetailSection>
       )}
       <DetailSection title={t`About Sky Ecosystem Tokens`}>
-        <Carousel
-          opts={{ loop: true, watchDrag: isMobileOrTablet }}
-          plugins={[
-            Autoplay({
-              delay: 5000,
-              stopOnInteraction: true,
-              stopOnMouseEnter: true,
-              stopOnFocusIn: true
-            }),
-            Fade()
-          ]}
-          className="relative"
-        >
-          <CarouselContent>
-            <CarouselItem>
-              <div className="pb-6">
-                <AboutUsds />
-              </div>
-            </CarouselItem>
-            {import.meta.env.VITE_RESTRICTED_BUILD !== 'true' && (
-              <CarouselItem>
-                <div className="pb-6">
-                  <AboutSUsds />
-                </div>
-              </CarouselItem>
-            )}
-            <CarouselItem>
-              <div className="pb-6">
-                <AboutSky />
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="pb-6">
-                <AboutSpk />
-              </div>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselControls className="absolute bottom-1 left-1/2 -translate-x-1/2" />
-        </Carousel>
+        <AboutTokensCarousel />
       </DetailSection>
       <DetailSection title={t`FAQs`}>
         <DetailSectionRow>
