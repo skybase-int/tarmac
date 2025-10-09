cae9a03a236cd19c18b240503caeff6e4cc90e3c
generatorpoint <generatorpoint@proton.me>
2025-10-09 12:51:39 -0600

    update cutoff date

--- packages/utils/src/tradeCutoffDates.ts
+++ packages/utils/src/tradeCutoffDates.ts
@@ -3,6 +3,6 @@ import { arbitrum, base } from 'wagmi/chains';
 // Cutoff dates for when each L2 switched from PSM to CowSwap
 // Trades before these dates will be fetched from PSM, after from CowSwap
 export const TRADE_CUTOFF_DATES: Record<number, Date> = {
-  [base.id]: new Date('2025-10-09T15:00:00Z'), // October 9, 2025 3pm UTC
-  [arbitrum.id]: new Date('2025-10-09T15:00:00Z') // October 9, 2025 3pm UTC
+  [base.id]: new Date('2025-10-09T19:30:00Z'), // October 9, 2025 19:30pm UTC
+  [arbitrum.id]: new Date('2025-10-09T19:30:00Z') // October 9, 2025 19:30pm UTC
 };
