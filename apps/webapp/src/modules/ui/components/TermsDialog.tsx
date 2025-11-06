72edbb28b0f4e107ecbe76bd21229287f94946dd
generatorpoint <generatorpoint@proton.me>
2025-11-06 14:25:36 -0700

    Revert "test linter"
    This reverts commit 36208cc80226bb137cf28a8c79ecb0cea48470bc.

--- apps/webapp/src/modules/ui/components/TermsDialog.tsx
+++ apps/webapp/src/modules/ui/components/TermsDialog.tsx
@@ -61,7 +61,6 @@ export const TermsDialog: React.FC<TermsDialogProps> = ({
       onAccept();
     }
   };
-  // making a change to test linter
 
   const dialogContent = showLoadingState ? (
     <DialogContent className="bg-containerDark max-w-[300px]">
