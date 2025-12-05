f1ff64acc2e71b3c4528d125c462534100c568bc
iceanddust <iceanddust@proton.me>
2025-12-05 18:32:30 -0300

    Add reusable hook to generate module URLs

--- apps/webapp/src/modules/layout/components/ConnectedModalTabs.tsx
+++ apps/webapp/src/modules/layout/components/ConnectedModalTabs.tsx
@@ -2,6 +2,10 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Trans } from '@lingui/react/macro';
 import { Text } from './Typography';
 import { ModuleCardVariant, ModulesBalances } from '@jetstreamgg/sky-widgets';
+import { getSupportedChainIds } from '@/data/wagmi/config/config.default';
+import { useChainId } from 'wagmi';
+import { useModuleUrls } from '@/modules/app/hooks/useModuleUrls';
+import { useConfigContext } from '@/modules/config/hooks/useConfigContext';
 
 enum ConnectedModalTabsEnum {
   SUPPLIED_FUNDS = 'supplied_funds',
@@ -10,6 +14,11 @@ enum ConnectedModalTabsEnum {
 }
 
 export function ConnectedModalTabs() {
+  const chainId = useChainId();
+  const { onExternalLinkClicked } = useConfigContext();
+
+  const { rewardsUrl, savingsUrlMap, sealUrl, stakeUrl, stusdsUrl } = useModuleUrls();
+
   return (
     <Tabs defaultValue={ConnectedModalTabsEnum.SUPPLIED_FUNDS}>
       <TabsList className="mb-6 grid w-full grid-cols-3">
@@ -24,7 +33,16 @@ export function ConnectedModalTabs() {
         </TabsTrigger>
       </TabsList>
       <TabsContent value={ConnectedModalTabsEnum.SUPPLIED_FUNDS} className="flex flex-col gap-6">
-        <ModulesBalances variant={ModuleCardVariant.alt} />
+        <ModulesBalances
+          variant={ModuleCardVariant.alt}
+          chainIds={getSupportedChainIds(chainId)}
+          rewardsCardUrl={rewardsUrl}
+          savingsCardUrlMap={savingsUrlMap}
+          sealCardUrl={sealUrl}
+          stakeCardUrl={stakeUrl}
+          stusdsCardUrl={stusdsUrl}
+          onExternalLinkClicked={onExternalLinkClicked}
+        />
       </TabsContent>
       <TabsContent value={ConnectedModalTabsEnum.WALLET_FUNDS}>
         <Text className="text-text">Wallet</Text>
