5d4582f9916b09101d729dd05438c378717400f1
bitsforpeace <bitsforpeace@proton.me>
2026-02-24 12:56:10 +0100

    Refactor VaultsAbout component to remove AboutUsds
    - Simplified the VaultsAbout component by removing the AboutUsds component, retaining only AboutMorphoVaults for a more focused presentation of vault information.

--- apps/webapp/src/modules/vaults/components/VaultsAbout.tsx
+++ apps/webapp/src/modules/vaults/components/VaultsAbout.tsx
@@ -1,11 +1,5 @@
-import { AboutUsds } from '@/modules/ui/components/AboutUsds';
 import { AboutMorphoVaults } from '@/modules/ui/components/AboutMorphoVaults';
 
 export function VaultsAbout() {
-  return (
-    <div>
-      <AboutMorphoVaults />
-      <AboutUsds />
-    </div>
-  );
+  return <AboutMorphoVaults />;
 }
