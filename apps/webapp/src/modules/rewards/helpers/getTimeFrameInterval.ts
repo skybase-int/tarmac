1a8af80ac91f4c45cfc4d107648bb90df74c4795
iceanddust <iceanddust@proton.me>
2026-03-05 18:27:19 -0300

    Use weekly interval for 1Y chart timeframe to smooth the line

--- apps/webapp/src/modules/rewards/helpers/getTimeFrameInterval.ts
+++ apps/webapp/src/modules/rewards/helpers/getTimeFrameInterval.ts
@@ -5,9 +5,10 @@ export const getTimeFrameInterval = (timeFrame: TimeFrame): number => {
   switch (timeFrame) {
     case 'w':
     case 'm':
-    case 'y':
     case 'all':
       return 86400; // 1 day in seconds
+    case 'y':
+      return 604800; // 1 week in seconds
     default:
       return 3600; // Default to 1 hour in seconds
   }
